c3f2f2c01f7fd6cf404b1203f3cb632f01954ca9
Rosa <rluquea95@gmail.com>
2026-04-06 13:29:24 +0200

    feat: implementa CartaPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
+import { Route, Routes } from 'react-router';
 import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
 import { HomePage } from './pages/HomePage';
+import { CartaPage } from './pages/CartaPage';
 
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col">
-      
+
       {/* Componente de navegación */}
       <Navbar />
 
-      {/* Landing Page*/}
-      <HomePage />
+      <Routes>
+        {/* Página de Inicio */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* Página de Carta */}
+        <Route path="/carta" element={<CartaPage />} />
+      </Routes>
 
       {/* Componente de pie de página */}
-      <Footer/>
+      <Footer />
     </div>
   )
 }
