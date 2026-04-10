ef7122879facf24e49db8001d45510a739a197d5
Rosa <rluquea95@gmail.com>
2026-04-10 11:21:17 +0200

    feat: agrega iconos Editar y Eliminar. Correcciones de tabulaciones en resto de componentes iconos

--- frontend/src/components/icons/CerrarIcon.jsx
+++ frontend/src/components/icons/CerrarIcon.jsx
@@ -1,13 +1,10 @@
 export const CerrarIcon = ({ className }) => {
-	return (
-		<svg xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 -960 960 960"
-			className={className}
-			fill="currentColor">
-			<path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
-		</svg>
-	);
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -960 960 960"
+      className={className}
+      fill="currentColor">
+      <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+    </svg>
+  );
 }
-
-
-
