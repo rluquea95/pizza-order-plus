aaf9f25cdd50950647071a11a2dd038e77a5987b
Rosa <rluquea95@gmail.com>
2026-04-29 14:21:02 +0200

    chore: modifica formato de imagenes de Landing a formato webp y se mejora su calidad

--- frontend/src/data/landingSlides.js
+++ frontend/src/data/landingSlides.js
@@ -1,21 +1,21 @@
 export const landingSlides = [
   {
     id: 0,
-    image: '/img/Landing/landing-1.jpg',
+    image: '/img/Landing/landing-1.webp',
     title: 'Pasión por la pizza',
     text: 'Nuestra masa madre artesanal reposa pacientemente para alcanzar la textura perfecta, complementada con ingredientes locales frescos y un horneado a la leña tradicional que captura siempre el verdadero espíritu de la pizza clásica.',
     buttonType: 'principal' // botones de Ver Carta e Iniciar Pedido
   },
   {
     id: 1,
-    image: '/img/Landing/landing-2.jpg',
+    image: '/img/Landing/landing-2.webp',
     title: 'Regístrate y disfruta',
     text: 'Realiza tu pedido cómodamente desde la web y escoge Recogida en local o Envío a domicilio.',
     buttonType: 'registro' // botón de registro
   },
   {
     id: 2,
-    image: '/img/Landing/landing-3.jpg',
+    image: '/img/Landing/landing-3.webp',
     title: 'Ingredientes frescos y de calidad',
     text: 'Seleccionamos cuidadosamente cada producto para que el resultado final en tu mesa sea una experiencia inolvidable en cada bocado.',
     buttonType: 'none' // No hay botones
